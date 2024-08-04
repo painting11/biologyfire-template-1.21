8c6d35c3e7adbf2ef417dfb66fdb7a06a322194e
painting <117021944+painting11@users.noreply.github.com>
2024-08-04 13:41:33 +0800

    Add files via upload

---
+++ src/main/java/name/biologyfire/mixin/FlintAndSteelItemMixin.java
@@ -0,0 +1,32 @@
+package name.biologyfire.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FlintAndSteelItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(FlintAndSteelItem.class)
+public class FlintAndSteelItemMixin extends Item {
+
+    public FlintAndSteelItemMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        World world = user.getWorld();
+        Vec3d Pos = entity.getPos();
+        entity.setFireTicks(20 * 5);
+        entity.setOnFire(true);
+        world.playSound(null, Pos.getX(), Pos.getY(), Pos.getZ(), SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
+        return ActionResult.success(world.isClient());
+    }
+}
